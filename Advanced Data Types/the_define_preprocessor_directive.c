cbbb18577f4a938bbd40650f00c647b5672369d6
George Calin <george.calin@gmail.com>
2021-11-08 12:22:42 +0100

    Advanced Data Types

---
+++ Advanced Data Types/the_define_preprocessor_directive.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define TWO_PI 2*3,1415
+#define PI 3.1415f
+
+/* =====================================================================
+ * Good practice: the #define preprocessor directive replaces at each occurrence the symbol with the value to which it is defined.
+ * Whilst the 'const float pi=3.14f' is datatype safe, by incorporating information about the data type too
+ * ===================================================================== */
+const float pi=3.1415f;
+
+int main(int argc, char **argv)
+{
+	
+	
+	return 0;
+}
---
+++ Advanced Data Types/using_typedef.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef int Counter;
+typedef int * i_pointer;
+typedef char* car;
+
+int main(void)
+{
+	Counter i, j;
+	i_pointer p; //same as int * p
+	
+	
+	car Dacia, Renault;
+	
+	return 0;
+}

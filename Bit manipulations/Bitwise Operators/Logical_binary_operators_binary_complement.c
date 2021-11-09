31355af9bee19b4249ab851243f7dbae6fd19a68
George Calin <george.calin@gmail.com>
2021-11-09 12:27:17 +0100

    Complement's operator binary

--- Bit manipulations/Bitwise Operators/Logical_binary_operators_binary_complement.c
+++ Bit manipulations/Bitwise Operators/Logical_binary_operators_binary_complement.c
@@ -9,6 +9,10 @@ int main(void)
      short int complement2=0;
 
 
+     /* =================================================
+     to obtain the decimal number from the binary complement= flip the bits , convert to binary and add +1
+     ================================================== */
+
     complement1=~number1; // 1101 0100 which is decimal -44
     complement2=~number2; // 1011 1110 which is decimal -66
     printf("The binary operation ~%d & %d ; \t ~%d  is %d.\n", number1,complement1, number2, complement2);

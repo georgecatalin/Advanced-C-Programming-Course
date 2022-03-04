ff8ce68737fd6d15d437f9bea67b2e208740e55a
George Calin <george.calin@gmail.com>
2022-03-04 10:43:54 +0200

    work in progress

--- otherfile.c
+++
@@ -1,8 +0,0 @@
-#include "otherfile.h"
-
-
-void printsome()
-{
-    dacia.speed=dacia.speed*10;
-    printf("the elements are %d ; %d ; %s .\n", dacia.speed,dacia.power, dacia.color);
-}
--- otherfile.h
+++
@@ -1,9 +0,0 @@
-#ifndef OTHERFILE_H_INCLUDED
-#define OTHERFILE_H_INCLUDED
-
-#include "types.h"
-extern thiscar dacia;
-
-void printsome();
-
-#endif // OTHERFILE_H_INCLUDED
